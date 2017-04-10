a29d04b7682f854b10de8f7069fc8c2b58e0974d
TheRegan <regan.ashworth@gmail.com>
2017-04-10 16:13:34 +1200

    Add Invoices endpoint
    and related endpoints and structs

---
+++ model/line_item.go
@@ -0,0 +1,38 @@
+package model
+
+//LineItem is a line containing detail on an Invoice
+type LineItem struct {
+
+	// Description needs to be at least 1 char long. A line item with just a description (i.e no unit amount or quantity) can be created by specifying just a <Description> element that contains at least 1 character
+	Description string `json:"Description,omitempty" xml:"Description,omitempty"`
+
+	// LineItem Quantity
+	Quantity string `json:"Quantity,omitempty" xml:"Quantity,omitempty"`
+
+	// LineItem Unit Amount
+	UnitAmount float32 `json:"UnitAmount,omitempty" xml:"UnitAmount,omitempty"`
+
+	// See Items
+	ItemCode string `json:"ItemCode,omitempty" xml:"ItemCode,omitempty"`
+
+	// See Accounts
+	AccountCode string `json:"AccountCode,omitempty" xml:"AccountCode,omitempty"`
+
+	// Used as an override if the default Tax Code for the selected <AccountCode> is not correct – see TaxTypes.
+	TaxType string `json:"TaxType,omitempty" xml:"TaxType,omitempty"`
+
+	// The tax amount is auto calculated as a percentage of the line amount (see below) based on the tax rate. This value can be overriden if the calculated <TaxAmount> is not correct.
+	TaxAmount float32 `json:"TaxAmount,omitempty" xml:"TaxAmount,omitempty"`
+
+	// If you wish to omit either of the <Quantity> or <UnitAmount> you can provide a LineAmount and Xero will calculate the missing amount for you. The line amount reflects the discounted price if a DiscountRate has been used . i.e LineAmount = Quantity * Unit Amount * ((100 – DiscountRate)/100)
+	LineAmount float32 `json:"LineAmount,omitempty" xml:"LineAmount,omitempty"`
+
+	// Optional Tracking Category – see Tracking.  Any LineItem can have a maximum of 2 <TrackingCategory> elements.
+	Tracking []TrackingCategory `json:"Tracking,omitempty" xml:"Tracking,omitempty"`
+
+	// Percentage discount being applied to a line item (only supported on ACCREC invoices – ACC PAY invoices and credit notes in Xero do not support discounts
+	DiscountRate string `json:"DiscountRate,omitempty" xml:"DiscountRate,omitempty"`
+
+	// The Xero identifier for a Repeating Invoicee.g. 297c2dc5-cc47-4afd-8ec8-74990b8761e9
+	RepeatingInvoiceID string `json:"RepeatingInvoiceID,omitempty" xml:"RepeatingInvoiceID,omitempty"`
+}
---
+++ model/tracking_option.go
@@ -0,0 +1,17 @@
+package model
+
+//TrackingOption is an option from within a Tracking category
+type TrackingOption struct {
+
+	// The Xero identifier for a tracking optione.g. ae777a87-5ef3-4fa0-a4f0-d10e1f13073a
+	TrackingOptionID string `json:"TrackingOptionID,omitempty" xml:"TrackingOptionID,omitempty"`
+
+	// The name of the tracking option e.g. Marketing, East (max length = 50)
+	Name string `json:"Name,omitempty" xml:"Name,omitempty"`
+
+	// The status of a tracking option
+	Status string `json:"Status,omitempty" xml:"Status,omitempty"`
+
+	// Filter by a tracking categorye.g. 297c2dc5-cc47-4afd-8ec8-74990b8761e9
+	TrackingCategoryID string `json:"TrackingCategoryID,omitempty" xml:"TrackingCategoryID,omitempty"`
+}
