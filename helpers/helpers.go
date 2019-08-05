0392087e17f3fb641efd0a317540988adfd31f67
Regan Ashworth <regan.ashworth@gmail.com>
2019-08-05 15:30:51 +1200

    Add ability to find and create history and notes

--- helpers/helpers.go
+++ helpers/helpers.go
@@ -121,3 +121,12 @@ func ReadPrivateKeyFromPath(privateKeyFilePath string) string {
 	}
 	return string(privateKeyFileContents)
 }
+
+func StringInSlice(str string, list []string) bool {
+ 	for _, v := range list {
+ 		if v == str {
+ 			return true
+ 		}
+ 	}
+ 	return false
+ }
